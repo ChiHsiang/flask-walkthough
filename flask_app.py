08cadb2d5c6b50810b734de28f281d5630a59ca4
Trey Cucco <tcucco@idibri.com>
2014-08-14 20:17:12 -0500

    Fleshes out the application a bit.

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/", methods=["GET"])
+def show_index():
+  return render_template("index.html")
+
+@app.route("/about", methods=["GET"])
+def show_about():
+  return render_template("about.html")
+
+@app.route("/sign-in", methods=["GET"])
+def show_sign_in():
+  return render_template("sign-in.html")
+
+@app.route("/sign-in", methods=["POST"])
+def sign_in():
+  raise NotImplementedError()
+
+@app.route("/sign-out", methods=["GET"])
+def sign_out():
+  raise NotImplementedError()
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
